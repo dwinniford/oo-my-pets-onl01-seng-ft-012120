0c5ef4e256858ad2c6c88f9ad102906e31f2cb62
Dave Winniford <dwinniford@gmail.com>
2020-01-29 18:57:04 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,11 +1,29 @@
 class Owner
-  attr_reader :name 
+  attr_reader :name, :species
   
-  @@pets = []
+  @@all = []
   
   def initialize(name)
     @name = name
+    @species = "human"
+    @pets = []
+    @@all << self 
   end 
   
+  def say_species
+    puts "Hello, I'm #{:species}"
+  end 
+  
+  def self.all 
+    @@all
+  end 
+  
+  def self.count
+    self.all.length 
+  end 
+  
+  def self.reset_all
+    @@all = []
+  end 
   
 end

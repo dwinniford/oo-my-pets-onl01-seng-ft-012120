ab885cf5c3cba5e1b219135dbb140b0ee25ee63b
Dave Winniford <dwinniford@gmail.com>
2020-01-29 19:02:27 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,15 @@
 class Cat
-  # code goes here
+  attr_reader :name, :mood 
+  attr_accessor :owner 
+  
+  @@all = []
+  
+  def initialize(name, owner)
+    @name = name 
+    @owner = owner 
+    @mood = "nervous"
+    @@all << self 
+  end 
+  
+  
 end
